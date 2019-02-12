14eca7788e16efe6dfa27d43455a2901b7c489c6
ml1150258 <mark.lehr@rcc.edu>
2019-02-11 20:24:52 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Book/README.md
@@ -0,0 +1,2 @@
+# 2019_Spring_CIS_CSC_17a
+Riverside City College Spring 2019 C++ Objects
---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on February 11th, 2019, 12:36 PM
+ * Purpose:  Creation of Template to be used for all
+ *           future projects
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    
+    //Initialize or input i.e. set variable values
+    
+    //Map inputs -> outputs
+    
+    //Display the outputs
+
+    //Exit stage right or left!
+    return 0;
+}
